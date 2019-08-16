e750692fea15d8968a457b1124600e0f3d54a074
Filiberto Reyes <filibertoreyes55@gmail.com>
2019-08-15 17:23:02 -0700

    24hrpainting local

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,335 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
+     <v-app light>
+    <v-toolbar class="white">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
     <v-content>
-      <HelloWorld/>
+      <section>
+        <v-parallax src="./assets/banner.jpg" height=400>
+          <v-layout
+            column
+            align-center
+            justify-center
+            class="white--text"
+          >
+            <h1 class="white--text mb-2 display-1 text-xs-center">Painting services based in Panorama City</h1>
+            <div class="subheading mb-3 text-xs-center">Open 24 Hours</div>
+            <v-btn
+              class="blue lighten-2 mt-5"
+              dark
+              large
+              href="/pre-made-themes"
+            >
+              Get Started
+            </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+          column
+          wrap
+          class="my-5"
+          align-center
+        >
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">What do we do?</h2>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md4>
+                  <v-card  class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">24 HR service</div>
+                    </v-card-title>
+                    <v-card-text>
+                      With a wide range of services that suit your every need and requirement,
+                      We at 24Hr Service have the know-how and the expertise to get the job done! Let us know how we can help,and we will guarantee
+                      your experience with our professionals is enjoyable and satisfactory. We love to hear from our costumers, so feel free to
+                      contact us with your feedback!
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Based in Socal</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Skip your search for house painters near you; contact the professional and industry experts . We serve the communities
+                      in Los Angeles, Thousand Oaks, Ventura, and neighboring cities in California. Contact us today to have the experts
+                      handle your exterior painting in summer.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Exterior House Painting</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Many homeowners wonder when the best time of year is to schedule exterior painting projects. While exterior painting
+                      can be done at nearly any time during the year (under the right conditions), the warmer months are usually the best
+                      time to take care of outdoor projects – including exterior painting in summer!
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+
+      <section>
+        <v-layout>
+          <v-flex xs12 sm10 offset-sm1>
+            <v-card>
+              <v-container grid-list-sm fluid>
+                <v-layout row wrap>
+                  <v-flex v-for="n in 9" :key="n" xs4 d-flex>
+                    <v-card class="d-flex">
+                      <v-img :src="require(`@/assets/gallery/pic (${n}).jpg`)" :max-height="225">
+                        <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+                          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-layout>
+                      </v-img>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+
+              <!-- <v-container grid-list-md>
+                <v-layout row wrap>
+                  <v-flex xs12 sm4>
+                    <v-card>
+                      <v-img :src="`./assets/yelp.jpg`"></v-img>
+                    </v-card>
+                  </v-flex>
+                  <v-flex xs12 sm8>
+                    <v-card>
+                      <v-card-title><h1>Visit our page on yelp!</h1></v-card-title>
+                      <v-layout>
+                        <v-flex xs12>
+                          <span class="yelp-review" data-review-id="sCrHLbqidbuAwtlayKZCNQ" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=TSGcKeXexKuPx5afe4oHig"
+                              rel="nofollow noopener">Amanda O.</a>'s <a href="https://www.yelp.com/biz/24hr-painting-services-los-angeles-2?hrid=sCrHLbqidbuAwtlayKZCNQ"
+                              rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/HmPl9VXWhH7c3yN95f10Ow" rel="nofollow noopener">24Hr
+                              painting services</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a>
+                            <script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script></span>
+                        </v-flex>
+                      </v-layout>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-container> -->
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </section>
+
+      <section>
+        <v-parallax src="./assets/color-paint-cans-dark.jpg" height="400">
+          <v-layout column align-center justify-center>
+            <div class="headline white--text mb-3 text-xs-center">Let us help you with your future painting projects.</div>
+            <v-btn
+              class="blue lighten-2 mt-5"
+              dark
+              large
+              href="/pre-made-themes"
+            >
+              Create an appointment
+            </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-container grid-list-xl>
+          <v-layout row wrap justify-center class="my-5">
+            <v-flex xs12 sm4>
+              <v-card class="elevation-0 transparent">
+                <v-toolbar dark class="blue darken-2">
+                  <v-toolbar-title>About</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  Fast, friendly, and fully functional, our professional Painting Service is well renowned throughout the San Fernando
+                  area. Our team is up for every job, managing projects with the skill and experience our clients have come to expect. We
+                  always stand behind our work, with customer satisfaction being our #1 priority. Contact us to learn more about our
+                  incredible staff and how they can help you!
+
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Stay Connected with us</div>
+                </v-card-title>
+                  <v-container>
+                    <v-layout justify-center>
+                      <v-flex xs12>
+                        <v-tooltip 
+                          bottom
+                          v-for="icon in icons">
+                          <v-btn
+                            :href="icon.url"
+                            :color="icon.color"
+                            target="blank"
+                            class="elevation-5"
+                            slot="activator"
+                            fab
+                            dark
+                          >
+                            <i :class="icon.name"></i>
+                          </v-btn>
+                          <span>{{icon.tooltip}}</span>
+                      </v-tooltip>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm4 offset-sm1>
+              <v-card class="elevation-0 transparent">
+                  <v-list two-line>
+                    <v-toolbar dark class="blue darken-2">
+                      <v-toolbar-title>Contact Information</v-toolbar-title>
+                    </v-toolbar>
+                    <v-list-item @click="">
+                      <v-list-item-action>
+                        <v-icon color="indigo">phone</v-icon>
+                      </v-list-item-action>
+                  
+                      <v-list-item-content>
+                        <v-list-item-title>(818) 647-5638</v-list-item-title>
+                        <v-list-item-subtitle>Work</v-list-item-subtitle>
+                      </v-list-item-content>
+                  
+                      <v-list-item-action>
+                        <v-icon>chat</v-icon>
+                      </v-list-item-action> 
+                    </v-list-item>
+
+                    <v-divider inset></v-divider>
+                  
+                    <v-list-item @click="">
+                      <v-list-item-action>
+                        <v-icon color="indigo">mail</v-icon>
+                      </v-list-item-action>
+                  
+                      <v-list-item-content>
+                        <v-list-item-title>javierlopezfredy199@gmail.com</v-list-item-title>
+                        <v-list-item-subtitle>Personal</v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                
+                  
+                    <v-divider inset></v-divider>
+                  
+                    <v-list-item @click="">
+                      <v-list-item-action>
+                        <v-icon color="indigo">location_on</v-icon>
+                      </v-list-item-action>
+                  
+                      <v-list-item-content>
+                        <v-list-item-title>15113 Burton St</v-list-item-title>
+                        <v-list-item-subtitle>Los Angeles, CA 91402
+                        Panorama City</v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+          <!-- TO DO: Finish Contact me Form -->
+          <!-- <v-layout row wrap>
+            <v-flex xs10 offset-sm1>
+              <v-card>
+                <v-toolbar dark class="blue darken-2">
+                  <v-toolbar-title>Send us an email!</v-toolbar-title>
+                </v-toolbar>
+                <v-form v-model="valid">
+                  <v-container>
+                    <v-layout align-space-between justify-center column fill-height>
+                      <v-flex xs12>
+                        <v-text-field v-model="name" :rules="nameRules" label="Name" required></v-text-field>
+                      </v-flex>
+          
+                      <v-flex xs12>
+                        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                      </v-flex>
+
+                      <v-flex xs12></v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-form>
+              </v-card>
+            </v-flex>
+          </v-layout> -->
+        </v-container>
+      </section>
+
+      <v-footer class="blue darken-2">
+        <v-layout row wrap align-center>
+          <v-flex xs12>
+            <div class="white--text ml-3">
+              2019
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-footer>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    HelloWorld
   },
   data: () => ({
     //
-  }),
+    title: "24HrPaintingServices",
+    icons: [
+      {
+        name: "fab fa-facebook fa-2x",
+        url: "https://www.facebook.com/24horas199/",
+        color: "blue",
+        tooltip: "Facebook"
+      },
+      {
+        name: "fab fa-instagram fa-2x",
+        url:
+          "https://instagram.com/javierlopezfred199?utm_source=ig_profile_share&igshid=u32rvjvhubrh",
+        color: "pink",
+        tooltip: "Instagram"
+      },
+      // { name: 'fab fa-yelp fa-2x',
+      //   url: 'https://www.yelp.com/biz/24hr-painting-services-los-angeles-2',
+      //   color: 'red',
+      //   tooltip:'Yelp' },
+      // { name: 'fas fa-thumbtack fa-2x',
+      //   url: 'https://www.thumbtack.com/-Van-Nuys-CA/service/2404480',
+      //   color: 'grey',
+      //   tooltip:'Thumbtack' },
+      {
+        name: "fab fa-houzz fa-2x",
+        url: "https://www.houzz.com/pro/fredy-navarro59/24hr-painting-services",
+        color: "green",
+        tooltip: "Houzz"
+      }
+    ]
+  })
 };
 </script>
